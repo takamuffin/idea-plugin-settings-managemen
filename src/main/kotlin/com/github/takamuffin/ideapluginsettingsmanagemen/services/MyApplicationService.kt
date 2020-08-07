179fa34eb783e1c458352922ecf0211db0656009
GitHub Action <action@github.com>
2020-08-07 19:57:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/takamuffin/ideapluginsettingsmanagemen/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.takamuffin.ideapluginsettingsmanagemen.services
+
+import com.github.takamuffin.ideapluginsettingsmanagemen.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
